d47130d0befe5cab5ba616232f04eb469265ae83
AdamGary <adam-gary@uiowa.edu>
2016-10-20 15:48:21 -0500

    Initial Commit

---
+++ Golf_Leaderboard_v1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Golf_Leaderboard_v1
+//
+//  Created by Adam Fairchild Gary on 10/18/16.
+//  Copyright © 2016 Group 3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
