39010c41d11c90b7fffd98e5e2938211a4017c1f
AdamGary <adam-gary@uiowa.edu>
2016-10-25 14:38:53 -0500

    vc's

---
+++ Golf_Leaderboard_v1/ScoreEntryViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ScoreEntryViewController.swift
+//  Golf_Leaderboard_v1
+//
+//  Created by Adam Fairchild Gary on 10/25/16.
+//  Copyright © 2016 Group 3. All rights reserved.
+//
+
+import UIKit
+
+class ScoreEntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
