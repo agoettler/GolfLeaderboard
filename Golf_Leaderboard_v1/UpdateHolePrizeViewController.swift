9a8bd9da7611600ca4e43a2e7980aa3144c3292f
AdamGary <adam-gary@uiowa.edu>
2016-10-26 16:47:21 -0500

    submit hole prize updates

---
+++ Golf_Leaderboard_v1/UpdateHolePrizeViewController.swift
@@ -0,0 +1,44 @@
+//
+//  UpdateHolePrizeViewController.swift
+//  Golf_Leaderboard_v1
+//
+//  Created by Adam Fairchild Gary on 10/26/16.
+//  Copyright © 2016 Group 3. All rights reserved.
+//
+
+import UIKit
+
+class UpdateHolePrizeViewController: UIViewController {
+
+    
+    @IBAction func claimHolePrizePressed(_ sender: UIButton) {
+        
+        // will update the current leader with the name of the player who pressed it
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
