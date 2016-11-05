009b84a437df47dec8cab07bc22299e862fed004
agoettler <agoettler@gmail.com>
2016-11-04 22:42:19 -0500

    Stubbed in simpler if statement for getNetScore of Scorecard

--- Golf_Leaderboard_v1/Scorecard.swift
+++ Golf_Leaderboard_v1/Scorecard.swift
@@ -8,7 +8,8 @@
 
 import Foundation
 
-public class Scorecard{
+public class Scorecard
+{
     
     let globals:CurrentEventGlobalAccess = CurrentEventGlobalAccess.globalData
     
@@ -22,7 +23,8 @@ public class Scorecard{
     // var netScoreDictionary: Dictionary<Int,Int> = [1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0,15:0,16:0,17:0,18:0]
 
 
-    public init(startHole: Int, handicap: Int, scorecard: [Int]){
+    public init(startHole: Int, handicap: Int, scorecard: [Int])
+    {
         //holes = globals.globalEvent.course.holes
         playerHandicap = handicap
         playerStartHole = startHole
@@ -30,7 +32,8 @@ public class Scorecard{
     }
     
     
-    public func updateScore(holeNumber: Int, grossScore: Int){
+    public func updateScore(holeNumber: Int, grossScore: Int)
+    {
         let hole:Hole = globals.globalEvent.course[holeNumber]
 
         let holeHandicap = hole.handicap
@@ -43,7 +46,8 @@ public class Scorecard{
     }
     
     
-    public func getNetScore(holeNumber: Int) -> Int{
+    public func getNetScore(holeNumber: Int) -> Int
+    {
         
         let hole:Hole = globals.globalEvent.course[holeNumber]
         let holeHandicap = hole.handicap
@@ -51,26 +55,37 @@ public class Scorecard{
         var netScore:Int = grossScoreArray[holeNumber]
 
         // calculate and update the player's net score
-        if(holeHandicap <= playerHandicap){ // if the hole handicap is < or = to the players handicap the player gets a stroke
+        if(holeHandicap <= playerHandicap)
+        { // if the hole handicap is < or = to the players handicap the player gets a stroke
             
             netScore = netScore - 1 // player gets a stroke take off for their net score on the hole
             
-            if(playerHandicap > 18){ // if a players handicap is over 18
+            if(playerHandicap > 18)
+            { // if a players handicap is over 18
                 let excessiveHandicap: Int = playerHandicap - 18 // find how many strokes over 18
-                if(holeHandicap <= excessiveHandicap){ // if hole handicap is less than or equal to the number of strokes over 18
+                if(holeHandicap <= excessiveHandicap)
+                { // if hole handicap is less than or equal to the number of strokes over 18
                     netScore = netScore - 1 // the player gets 2 strokes on the hole, remove a second stroke for that hole
                 }
             }
+            /*
+            if ( playerHandicap > 18 && hole.handicap <= (playerHandicap - 18))
+            {
+                netScore -= 1
+            }
+            */
         }
         
         return netScore
     }
     
     
-    public func getGrossStrokesOverPar() -> Int {
+    public func getGrossStrokesOverPar() -> Int
+    {
         var totalGrossScore: Int = 0
         
-        for aHole in grossScoreArray{
+        for aHole in grossScoreArray
+        {
             totalGrossScore += aHole
         }
         
@@ -79,11 +94,13 @@ public class Scorecard{
     
     
     
-    public func getNetStrokesOverPar() -> Int {
+    public func getNetStrokesOverPar() -> Int
+    {
         var totalNetScore: Int = 0
         var net:Int = 0
         
-        for index in 0..<grossScoreArray.count{
+        for index in 0..<grossScoreArray.count
+        {
             net = getNetScore(holeNumber: index)
             totalNetScore += net
         }
@@ -94,7 +111,7 @@ public class Scorecard{
     
     // TODO: Decide if the subscript should be 0-indexed or 1-indexed
     public subscript(index: Int) -> (hole: Hole, grossScore: Int, netScore: Int)
-        {
+    {
         get
         {
             assert((index >= 1 && index <= grossScoreArray.count), "Index out of range")
